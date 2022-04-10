4a6c76e4f1479b5e65a444dde94224199750d92b
nikolatx <nikolatx@gmail.com>
2022-04-10 18:12:40 +0200

    Adding salted PasswordEncodingTest

---
+++ src/test/java/guru/sfg/brewery/web/controllers/PasswordEncodingTest.java
@@ -0,0 +1,21 @@
+package guru.sfg.brewery.web.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.util.DigestUtils;
+
+import java.nio.charset.StandardCharsets;
+
+public class PasswordEncodingTest {
+
+    static final String PASSWORD = "password";
+
+    @Test
+    void hashingExample() {
+        System.out.println(DigestUtils.md5DigestAsHex(PASSWORD.getBytes()));
+        System.out.println(DigestUtils.md5DigestAsHex(PASSWORD.getBytes()));
+
+        String salted = PASSWORD + "ThisIsMySaltValue";
+        System.out.println(DigestUtils.md5DigestAsHex(salted.getBytes()));
+    }
+
+}
